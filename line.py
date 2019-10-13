6894ce092d08428dd63a791fca8463792806052c
EcsFlash <55760802+EcsFlash@users.noreply.github.com>
2019-10-13 10:06:14 +0300

    Add files via upload

---
+++ line.py
@@ -0,0 +1,22 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+x = 0
+y = 0
+n = 0
+m = 0
+g = (a * x)
+h = (b * y)
+j = (c * x)
+k = (d * y)
+n = g + h
+g = e - h
+x = g / a
+m = (c * x) + (d * y)
+k = f - (c * x)
+y = k / d
+print(y)
+print(x)
